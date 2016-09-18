d542a45d055bbd3c1fe725e251260c71986cf6e8
Kevin Ma <kbma.kevin@gmail.com>
2016-09-17 23:26:34 -0400

    Rotating a Square 360 Degrees UsingTween

--- GraphicsDemo/Scripts/core/game.ts
+++ GraphicsDemo/Scripts/core/game.ts
@@ -8,6 +8,24 @@
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60;
         createjs.Ticker.on('tick', gameLoop);
+
+        // semi-transparent screen
+        let screen = new createjs.Shape()
+        screen.graphics.beginFill(createjs.Graphics.getRGB(0, 0, 0, 0.6))
+            .drawRect(0, 0, stage.canvas.width, stage.canvas.height)
+        stage.addChild(screen)
+
+        let g = new createjs.Graphics()
+            .beginStroke('#000')
+            .beginFill('#f00')
+            .drawRect(0, 0, 100, 100);
+
+        let square: createjs.Shape = new createjs.Shape(g);
+        square.x = 150;
+        square.y = 100;
+        stage.addChild(square)
+        
+        createjs.Tween.get(square).to({ rotation: 360 }, 3000);
     }
 
     function gameLoop(): void {
@@ -16,58 +34,7 @@
     }
 
     function main(): void {
-        // semi-transparent screen
-        let screen = new createjs.Shape()
-        screen.graphics.beginFill(createjs.Graphics.getRGB(0, 0, 0, 0.6))
-            .drawRect(0, 0, stage.canvas.width, stage.canvas.height)
-        stage.addChild(screen)
-
-        // RECTANGLE
-        let rect = new createjs.Shape()
-        rect.graphics.beginStroke('#000')
-        rect.graphics.beginFill('#f00')
-        rect.graphics.drawRect(0, 0, 150, 100)
-        rect.x = rect.y = 20;
-        stage.addChild(rect)
-
-        //CIRCLE
-        let circle = new createjs.Shape()
-        circle.graphics.beginStroke('#000')
-        circle.graphics.beginFill('#f00')
-        circle.graphics.drawCircle(0, 0, 50)
-        circle.x = 250
-        circle.y = 70
-        stage.addChild(circle)
-
-        //STAR
-        let poly = new createjs.Shape();
-        poly.graphics.beginStroke('#000');
-        poly.graphics.beginFill('#90ABC2');
-        poly.graphics.drawPolyStar(0, 0, 60, 6, 0.6, 20);
-        poly.x = 400;
-        poly.y = 70
-        stage.addChild(poly)
-
-        //TRIANGLE
-        let tri = new createjs.Shape();
-        tri.graphics.beginStroke('#000');
-        tri.graphics.beginFill('#0f0')
-        tri.graphics.moveTo(50, 0)
-            .lineTo(0, 100)
-            .lineTo(100, 100)
-            .lineTo(50, 0);
-        tri.x = 20
-        tri.y = 150
-        stage.addChild(tri)
-
-        //ROUNDED RECTANGLE
-        let roundedRectangle = new createjs.Shape()
-        roundedRectangle.graphics.beginStroke('#000')
-        roundedRectangle.graphics.beginFill('#f7d0d1')
-        roundedRectangle.graphics.drawRoundRect(0, 0, 400, 100, 10)
-        roundedRectangle.x = roundedRectangle.y = 150
-        stage.addChild(roundedRectangle)
-
+        
     }
 
     window.onload = setupStage;
